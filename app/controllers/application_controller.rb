a85e871e2eb8c86b20502f401541f2bff26aef20
Kate <kbalducci@hotmail.com>
2015-01-29 13:52:04 -0600

    updated routes and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-end
+  before_filter :cors_set_access_control_headers
 
-  before_filter :cors_set_access_control_headers
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin,  X-Requested-With, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Request-Method'] = "*"
-  end
-
+    def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token'
+      headers['Access-Control-Request-Method'] = '*'
+    end
+  end
