4e8dc667e4e8fe2275cd5824af20ad1a97ebadf5
Kate <kbalducci@hotmail.com>
2015-01-29 16:05:19 -0600

    updated created_at

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -28,7 +28,7 @@ class ChatsController < ApplicationController
 
     respond_to do |format|
       if @chat.save
-        format.html { redirect_to @chat, notice: 'Chat was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Chat was successfully created.' }
         format.json { render json, status: :created, location: @chat }
       else
         format.html { render :new }
--- app/views/chats/index.json.jbuilder
+++ app/views/chats/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@chats) do |chat|
-  json.extract! chat, :id, :name, :message, :like, :photo
+  json.extract! chat, :id, :name, :message, :like, :photo, :created_at
   json.url chat_url(chat, format: :json)
 end
